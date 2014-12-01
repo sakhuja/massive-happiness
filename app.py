d0709a66de425055321f2e59cad0393221fdfeb5
Aditya Sakhuja <asakhuja@yahoo-inc.com>
2014-11-30 23:22:03 -0800

    adding webpage

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    # return "Hello World!"
+    return render_template('main/index.html')
 
 if __name__ == "__main__":
     app.run()
